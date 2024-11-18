2fc05edc65d83589da8174fcff375ae8af259d58
tntcannon5000 <niranjan.kewalramani@outlook.com>
2024-11-18 16:26:02 +0000

    Integrated FastAPI calling from Rust main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+
 fn main() {
+    Command::new("python")
+        .arg("-m")
+        .arg("uvicorn")
+        .arg("main:app")  // Just reference the module directly
+        .arg("--reload")
+        .current_dir("../src-fastapi")  // Change the working directory to src-fastapi
+        .spawn()
+        .expect("Failed to start uvicorn server");
+
     dcstoragetauri_lib::run()
 }
